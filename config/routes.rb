4f6dbd1195ddf7ef5ebb963e06eff659b007f87b
Thembi Baleni <thembi.baleni@gmail.com>
2021-04-16 15:51:53 +1000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  resources :users, only: [:index, :create, :update, :destroy]
+  resources :my_vaccines, only: [:index, :create]
+  resources :vaccines, only: [:index, :show]
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# Rails.application.routes.draw do
+
+#   resources :users, only: [:index, :create, :update, :destroy] 
+  
+#   resources :my_vaccines, only: [:index, :create]
+  
+#   resources :vaccines, only: [:index, :show]
+  
+#   get '/profile', to: 'users#show'
+#   post '/login', to: 'auth#create'
+
+#   root "welcome#index"
+# end
